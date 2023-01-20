448d7ba1c7dbada0596322e40754f0142ac2ae70
Eleni Voreopoulou <evoreopoulou@wings-ict-solution.eu>
2023-01-20 17:02:20 +0200

    Third commit

--- app/src/main/java/com/enalymounioz/stackoverflowqueryapp/model/StockOverflowApi.kt
+++ app/src/main/java/com/enalymounioz/stackoverflowqueryapp/model/StockOverflowApi.kt
@@ -2,9 +2,13 @@ package com.enalymounioz.stackoverflowqueryapp.model
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StockOverflowApi {
     @GET("/2.3/questions?order=desc&sort=votes&tagged=android&site=stackoverflow")
     fun getQuestions(@Query("page") page: Int): Call<ResponseWrapper<Question>>
+
+    @GET("/2.3/questions/{id}/answers?order=desc&sort=votes&site=stackoverflow")
+    fun getAnswers(@Path("id")questionId: Int, @Query("page") page: Int):Call<ResponseWrapper<Answer>>
 }
