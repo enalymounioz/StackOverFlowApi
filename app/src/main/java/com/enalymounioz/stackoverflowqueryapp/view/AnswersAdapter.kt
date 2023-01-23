6e24c8ce914bb9d22c3beb481fbc2060d39fb6aa
Eleni Voreopoulou <evoreopoulou@wings-ict-solution.eu>
2023-01-23 12:20:23 +0200

    Asnwers Function

---
+++ app/src/main/java/com/enalymounioz/stackoverflowqueryapp/view/AnswersAdapter.kt
@@ -0,0 +1,39 @@
+package com.enalymounioz.stackoverflowqueryapp.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.enalymounioz.stackoverflowqueryapp.R
+import com.enalymounioz.stackoverflowqueryapp.model.Answer
+import kotlinx.android.synthetic.main.answer_layout.view.*
+
+class AnswersAdapter(val answers: ArrayList<Answer>): RecyclerView.Adapter<AnswersAdapter.AdapterViewHolder>() {
+
+    fun addAnswers(newAnswers: List<Answer>){
+        val currentLength = answers.size
+        answers.addAll(newAnswers)
+        notifyItemInserted(currentLength)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
+        AdapterViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.answer_layout, parent, false)
+        )
+
+    override fun getItemCount()= answers.size
+
+    override fun onBindViewHolder(holder: AdapterViewHolder, position: Int) {
+        holder.bind(answers[position])
+    }
+
+    class AdapterViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val title = view.item_answer
+        fun bind(answer: Answer){
+            title.text = answer.toString()
+
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/enalymounioz/stackoverflowqueryapp/viewmodel/DetailViewModel.kt
@@ -0,0 +1,56 @@
+package com.enalymounioz.stackoverflowqueryapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.enalymounioz.stackoverflowqueryapp.model.Answer
+import com.enalymounioz.stackoverflowqueryapp.model.ResponseWrapper
+import com.enalymounioz.stackoverflowqueryapp.model.StackOverflowService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DetailViewModel: ViewModel() {
+    val answersResponse = MutableLiveData<List<Answer>>()
+    val loading = MutableLiveData<Boolean>()
+    val error = MutableLiveData<String?>()
+
+    var questionId = 0
+    var page = 0
+
+    fun getNextPage(questionId: Int?) {
+        questionId?.let {
+            this.questionId = it
+            page++
+            getAnswers()
+        }
+    }
+
+    private fun getAnswers(){
+        StackOverflowService.api.getAnswers(questionId, page)
+            .enqueue(object : Callback<ResponseWrapper<Answer>>{
+                override fun onResponse(
+                    call: Call<ResponseWrapper<Answer>>,
+                    response: Response<ResponseWrapper<Answer>>
+                ) {
+                    val answers = response.body()
+                    answers?.let {
+                        answersResponse.value = it.items
+                        loading.value = false
+                        error.value = null
+                    }
+
+
+                }
+
+                override fun onFailure(call: Call<ResponseWrapper<Answer>>, t: Throwable) {
+
+                }
+
+            })
+    }
+    private fun onError(message: String){
+        error.value = message
+        loading.value = false
+    }
+
+}
